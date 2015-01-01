3bb83d99b426e536edef15e85627c4535473d2a7
lina1812 <alena18.12.1992@mail.ru>
2015-01-01 22:54:48 +0300

    frrst

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dive::Application.config.secret_key_base = '25cc14844fbaec14f937c3ea24f29936c19c59f53d35f1a0e7fcc29fdcd88811b77ee21abbff43761c2b066451f89c4f4b01db53e085f6b20c859066cab5ebca'
